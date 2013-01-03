31deab00db69e8c799d7ac7ac191632067595a88
Engine Games <engine.games@gmail.com>
2013-01-03 21:01:03 +0000

    Updated all copyright notices to 2013.
    git-svn-id: http://tmx-parser.googlecode.com/svn/trunk@47 92f78ae9-c003-d0a5-1fdc-3c6e6cf79373

--- TmxParser/TmxTileset.cpp
+++ TmxParser/TmxTileset.cpp
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // TmxTileset.cpp
 //
-// Copyright (c) 2010-2012, Tamir Atias
+// Copyright (c) 2010-2013, Tamir Atias
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
