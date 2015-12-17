f8c9b6bb0c3a5973c3c9f17fe37072a2d1513146
Andrew Kelley <superjoe30@gmail.com>
2015-12-16 18:22:30 -0700

    build: fix tinyxml2 finder

--- cmake/FindTinyXML2.cmake
+++ cmake/FindTinyXML2.cmake
@@ -1,30 +1,15 @@
-# This file licensed under GNU GPL v3
-# Source: https://code.google.com/p/mgep/source/browse/branches/clientv2/CMakeModules/FindTinyXML.cmake
+# Copyright (c) 2014 Andrew Kelley
+# This file is MIT licensed.
+# See http://opensource.org/licenses/MIT
 
-# - Find TinyXML
-# Find the native TinyXML includes and library
-#
-#   TINYXML2_FOUND       - True if TinyXML2 found.
-#   TINYXML2_INCLUDE_DIR - where to find tinyxml2.h, etc.
-#   TINYXML2_LIBRARIES   - List of libraries when using TinyXML2.
-#
+# TINYXML2_FOUND
+# TINYXML2_INCLUDE_DIR
+# TINYXML2_LIBRARIES
 
-IF( TINYXML2_INCLUDE_DIR )
-    # Already in cache, be silent
-    SET( TinyXML2_FIND_QUIETLY TRUE )
-ENDIF( TINYXML2_INCLUDE_DIR )
+find_path(TINYXML2_INCLUDE_DIR NAMES tinyxml2.h)
+find_library(TINYXML2_LIBRARIES NAMES tinyxml2)
 
-FIND_PATH( TINYXML2_INCLUDE_DIR "tinyxml2.h"
-           PATH_SUFFIXES "tinyxml2" )
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TINYXML2 DEFAULT_MSG TINYXML2_LIBRARIES TINYXML2_INCLUDE_DIR)
 
-FIND_LIBRARY( TINYXML2_LIBRARIES
-              NAMES "tinyxml2"
-              PATH_SUFFIXES "tinyxml2" )
-
-# handle the QUIETLY and REQUIRED arguments and set TINYXML2_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "TinyXML2" DEFAULT_MSG TINYXML2_INCLUDE_DIR TINYXML2_LIBRARIES )
-
-MARK_AS_ADVANCED( TINYXML2_INCLUDE_DIR TINYXML2_LIBRARIES )
-
+mark_as_advanced(TINYXML2_INCLUDE_DIR TINYXML2_LIBRARIES)
